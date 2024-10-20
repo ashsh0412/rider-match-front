087f022a3e1091f365fc95f2866f43c5d50da305
Sungho Baek <ashsh0412@gmail.com>
2024-10-20 15:25:48 -0400

    signup form fetching to backend

---
+++ src/SignUpForm.tsx
@@ -0,0 +1,228 @@
+import {
+  Flex,
+  Box,
+  FormControl,
+  FormLabel,
+  Input,
+  InputGroup,
+  HStack,
+  InputRightElement,
+  Stack,
+  Button,
+  Heading,
+  Text,
+  useColorModeValue,
+  Link,
+  useToast, // useToast 훅을 추가
+} from "@chakra-ui/react";
+import { useEffect, useState } from "react";
+import { ViewIcon, ViewOffIcon } from "@chakra-ui/icons";
+
+export default function SignupCard() {
+  const toast = useToast(); // useToast 훅을 초기화
+  const [showPassword, setShowPassword] = useState(false);
+  const [formData, setFormData] = useState({
+    firstName: "",
+    lastName: "",
+    email: "",
+    password: "",
+    username: "",
+  });
+
+  useEffect(() => {
+    fetch("http://127.0.0.1:8000/api/v1/users/");
+  }, []);
+
+  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const { id, value } = e.target;
+    setFormData({ ...formData, [id]: value });
+  };
+
+  const handleSubmit = async () => {
+    const { firstName, lastName, email, password, username } = formData;
+
+    if (!firstName || !lastName || !password || !username) {
+      toast({
+        title: "All required fields must be filled.",
+        status: "warning",
+        position: "top-right",
+        isClosable: true,
+      });
+      return;
+    }
+
+    // Username length check
+    if (username.length < 4) {
+      toast({
+        title: "Username must be at least 4 characters long.",
+        status: "warning",
+        position: "top-right",
+        isClosable: true,
+      });
+      return;
+    }
+
+    // Password length check
+    if (password.length < 8) {
+      toast({
+        title: "Password must be at least 8 characters long.",
+        status: "warning",
+        position: "top-right",
+        isClosable: true,
+      });
+      return;
+    }
+
+    // Email format check (basic regex check)
+    const emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    if (email && !emailPattern.test(email)) {
+      toast({
+        title: "Invalid email format.",
+        status: "warning",
+        position: "top-right",
+        isClosable: true,
+      });
+      return;
+    }
+
+    try {
+      const response = await fetch("http://127.0.0.1:8000/api/v1/users/", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          first_name: firstName,
+          last_name: lastName,
+          email: email,
+          password: password,
+          username: username,
+        }),
+      });
+
+      if (response.ok) {
+        toast({
+          title: "User created successfully!",
+          status: "success",
+          position: "top-right",
+          isClosable: true,
+        });
+      } else {
+        toast({
+          title: "A user with that username already exists.",
+          status: "error",
+          position: "top-right",
+          isClosable: true,
+        });
+      }
+    } catch (error) {
+      toast({
+        title: "Error occurred while creating user.",
+        status: "error",
+        position: "top-right",
+        isClosable: true,
+      });
+    }
+  };
+
+  return (
+    <Flex
+      minH={"100vh"}
+      align={"center"}
+      justify={"center"}
+      bg={useColorModeValue("gray.50", "gray.800")}
+    >
+      <Stack spacing={8} mx={"auto"} maxW={"lg"} py={12} px={6}>
+        <Stack align={"center"}>
+          <Heading fontSize={"4xl"} textAlign={"center"}>
+            Sign up
+          </Heading>
+        </Stack>
+        <Box
+          rounded={"lg"}
+          bg={useColorModeValue("white", "gray.700")}
+          boxShadow={"lg"}
+          p={8}
+        >
+          <Stack spacing={4}>
+            <HStack>
+              <Box>
+                <FormControl id="firstName" isRequired>
+                  <FormLabel>First Name</FormLabel>
+                  <Input
+                    type="text"
+                    value={formData.firstName}
+                    onChange={handleInputChange}
+                  />
+                </FormControl>
+              </Box>
+              <Box>
+                <FormControl id="lastName" isRequired>
+                  <FormLabel>Last Name</FormLabel>
+                  <Input
+                    type="text"
+                    value={formData.lastName}
+                    onChange={handleInputChange}
+                  />
+                </FormControl>
+              </Box>
+            </HStack>
+            <FormControl id="username" isRequired>
+              <FormLabel>Username</FormLabel>
+              <Input
+                type="text"
+                value={formData.username}
+                onChange={handleInputChange}
+              />
+            </FormControl>
+            <FormControl id="email">
+              <FormLabel>Email address</FormLabel>
+              <Input
+                type="email"
+                value={formData.email}
+                onChange={handleInputChange}
+              />
+            </FormControl>
+            <FormControl id="password" isRequired>
+              <FormLabel>Password</FormLabel>
+              <InputGroup>
+                <Input
+                  type={showPassword ? "text" : "password"}
+                  value={formData.password}
+                  onChange={handleInputChange}
+                />
+                <InputRightElement h={"full"}>
+                  <Button
+                    variant={"ghost"}
+                    onClick={() =>
+                      setShowPassword((showPassword) => !showPassword)
+                    }
+                  >
+                    {showPassword ? <ViewIcon /> : <ViewOffIcon />}
+                  </Button>
+                </InputRightElement>
+              </InputGroup>
+            </FormControl>
+            <Button
+              loadingText="Submitting"
+              size="lg"
+              bg={"black"}
+              color={"white"}
+              _hover={{
+                bg: "gray.500",
+              }}
+              onClick={handleSubmit} // Submit form on click
+            >
+              Sign up
+            </Button>
+            <Stack pt={6}>
+              <Text align={"center"}>
+                Already a user? <Link color={"blue.400"}>Login</Link>
+              </Text>
+            </Stack>
+          </Stack>
+        </Box>
+      </Stack>
+    </Flex>
+  );
+}
--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import { ChakraProvider } from "@chakra-ui/react";
-import SignupCard from "./LoginForm";
+import SignupCard from "./SignUpForm";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
