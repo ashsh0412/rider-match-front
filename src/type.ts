4c4c0264d9d4b0231f29ac132abf650bb8c8cc79
Sungho Baek <ashsh0412@gmail.com>
2024-11-20 15:49:55 -0500

    허ㅏ

--- src/type.ts
+++ src/type.ts
@@ -278,7 +278,7 @@ export interface LocationCard {
 export const BASE_URL =
   process.env.NODE_ENV === "development"
     ? "http://127.0.0.1:8000/api/v1/"
-    : "https://drivermatch.store/";
+    : "https://backend.drivermatch.store/api/v1/";
 
 export const KAKAO_REDIRECT_URL =
   process.env.NODE_ENV === "development"
